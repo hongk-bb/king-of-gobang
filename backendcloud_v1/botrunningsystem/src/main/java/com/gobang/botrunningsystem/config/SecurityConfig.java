319c3849c876f332e0f1b2006b45de477c55c2fe
Xavi Lee <awxiaoxian2020@163.com>
2023-07-10 16:12:08 +0800

    upgrade to JDK17 and Spring Boot 3.0

---
+++ backendcloud_v1/botrunningsystem/src/main/java/com/gobang/botrunningsystem/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.gobang.botrunningsystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * 实现用户密码的加密存储, 放行登录，注册等接口
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeRequests(authorize -> authorize
+                        .requestMatchers("/bot/add/").hasIpAddress("127.0.0.1")
+                        .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+}
+
+//                .requestMatchers("/bot/add/").hasIpAddress("127.0.0.1")  // 只允许本地访问
